177620d4c56ed8003e8b77e3a5f11ac8d5f60fb5
Shrey Gupta <connectwithshrey@gmail.com>
2019-03-06 23:12:42 +0530

    Updates

---
+++ precache-manifest.7a7bf40aff9bb216c51770c911d02f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d222074ba9cef424bd4",
+    "url": "/PhoneDirectory-SPA-React/static/js/runtime~main.1d222074.js"
+  },
+  {
+    "revision": "98b8f434865dc30e327e",
+    "url": "/PhoneDirectory-SPA-React/static/js/main.98b8f434.chunk.js"
+  },
+  {
+    "revision": "6fcd181c8166b8deef33",
+    "url": "/PhoneDirectory-SPA-React/static/js/2.6fcd181c.chunk.js"
+  },
+  {
+    "revision": "98b8f434865dc30e327e",
+    "url": "/PhoneDirectory-SPA-React/static/css/main.9fca4416.chunk.css"
+  },
+  {
+    "revision": "6fcd181c8166b8deef33",
+    "url": "/PhoneDirectory-SPA-React/static/css/2.29947851.chunk.css"
+  },
+  {
+    "revision": "74625f588d107d5f2e368290abc2c6b9",
+    "url": "/PhoneDirectory-SPA-React/index.html"
+  }
+];
